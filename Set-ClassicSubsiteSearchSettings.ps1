5cd9194039fe6afa399e8575c50feadde80995d1
koskila <posti@koskila.net>
2019-06-18 13:46:51 +0300

    Create Set-ClassicSubsiteSearchSettings.ps1

---
+++ Set-ClassicSubsiteSearchSettings.ps1
@@ -0,0 +1,32 @@
+#This script sets the search center URL as well as the seach results page URL for a given site collection
+
+function Set-ClassicWebSearchSettings ($web, $searchUrl, $inh, $showNav) {
+    # Write-Host $web.Title $web.URL $searchUrl $inh $showNav
+    Write-Host $web.Title ( $web.URL ) -ForegroundColor White
+    Set-PnPPropertyBagValue -Web $web -Key SRCH_ENH_FTR_URL_SITE -Value $searchUrl
+    Set-PnPPropertyBagValue -Web $web -Key SRCH_SB_SET_SITE `
+        -Value '{"Inherit":$inh,"ResultsPageAddress":$searchUrl,"ShowNavigation":$showNav}'
+}
+
+function Set-ClassicSiteSearchSettings ($siteUrl, $searchResultsPageAbsoluteUrl, $inherit, $showNavigation, $recurse) {
+
+    $cred = Get-Credential
+    $ctx = Connect-PnPOnline -Url $siteUrl -Credentials $cred
+    $web = Get-PnPWeb
+
+    # in PnP PowerShell you don't ACTUALLY have to recurse. Just pass it as a parameter
+    if ($recurse -eq $true) {
+        $allSites = Get-PnPSubWebs -Recurse
+    }
+
+    $allSites += $web
+
+    Write-Host "Setting values to: " $searchResultsPageAbsoluteUrl $inherit $showNavigation
+
+    $allSites | ForEach-Object { 
+        Set-ClassicWebSearchSettings $_ $searchResultsPageAbsoluteUrl $inherit $showNavigation
+    }
+}
+
+# Example
+Set-ClassicSiteSearchSettings "https://simedev.sharepoint.com/sites/valo1.15.1/" "/search/pages/results.aspx" $true $true $false
